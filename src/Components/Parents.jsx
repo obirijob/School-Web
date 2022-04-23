ef788ca26518a8f185b8797adc139d66acaac820
obirijob <82807624+obirijob@users.noreply.github.com>
2022-04-23 11:17:04 +0300

    Add Parent

--- src/Components/Parents.jsx
+++ src/Components/Parents.jsx
@@ -2,10 +2,17 @@ import React, { useState, useEffect } from "react"
 import Search from "./Search"
 import LoadBE from "../Helpers/LoadBE"
 import { DotFilledIcon, PlusIcon } from "@radix-ui/react-icons"
+import Modal from "./Modal"
 
 function Parents() {
   const [students, setStudents] = useState([])
   const [waiting, setWaiting] = useState(false)
+  const [addModal, setAddModal] = useState(false)
+  const [name, setName] = useState("")
+  const [phone, setPhone] = useState("")
+  const [email, setEmail] = useState("")
+  const [isMale, setIsMale] = useState(true)
+  const [error, setError] = useState(null)
   useEffect(() => {
     loadStd()
   }, [])
@@ -30,7 +37,7 @@ function Parents() {
       <div className="title">Parents</div>
       <div className="options">
         <Search placeholder="Find a Parent" />
-        <div className="option">
+        <div className="option" onClick={() => setAddModal(true)}>
           <PlusIcon />
           <span>Add Parent</span>
         </div>
@@ -40,13 +47,81 @@ function Parents() {
           <DotFilledIcon className="grow-anim" />
         </>
       ) : (
-        <div className="list">
-          {students.map(({ email, name }) => (
-            <div className="student" key={email + "parent"}>
-              <div className="name">{name}</div>
-            </div>
-          ))}
-        </div>
+        <>
+          <Modal
+            shown={addModal}
+            title="Add Parent"
+            hide={() => setAddModal(false)}
+          >
+            <form
+              onSubmit={async e => {
+                e.preventDefault()
+                setError(null)
+                let { errors, data } = await LoadBE(`mutation{
+                  addParent(name:"${name}", phone: ${phone}, email: "${email}", male: ${isMale}){
+                    name
+                  }
+                }`)
+                if (errors) {
+                  setError(errors)
+                } else {
+                  loadStd()
+                  alert(
+                    `${data.addParent.name.toUpperCase()} successfuly added to the system`
+                  )
+                }
+              }}
+            >
+              {error && (
+                <div className="error">
+                  {error.map(e => (
+                    <span>{e.message}</span>
+                  ))}
+                </div>
+              )}
+              <div className="input">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  id="name"
+                  onInput={e => setName(e.target.value)}
+                />
+              </div>
+              <div className="input">
+                <label htmlFor="phone">Phone</label>
+                <input
+                  type="text"
+                  id="phone"
+                  onInput={e => setPhone(e.target.value)}
+                />
+              </div>
+              <div className="input">
+                <label htmlFor="email">Email Address</label>
+                <input
+                  type="text"
+                  id="email"
+                  onInput={e => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="input">
+                <label htmlFor="isMale">Is Male?</label>
+                <input
+                  type="checkbox"
+                  defaultChecked={isMale}
+                  onInput={e => setIsMale(e.target.checked)}
+                />
+              </div>
+              <button className="option">Add</button>
+            </form>
+          </Modal>
+          <div className="list">
+            {students.map(({ email, name }) => (
+              <div className="student" key={email + "parent"}>
+                <div className="name">{name}</div>
+              </div>
+            ))}
+          </div>
+        </>
       )}
     </div>
   )
